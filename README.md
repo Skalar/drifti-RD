da1eae51ad371c682719cd3e3082169ab72e2dff
joshclement0 <43146365+joshclement0@users.noreply.github.com>
2022-06-20 13:22:24 +0200

    Combination
    This is now the combination of all drifti R&D projects

--- README.md
+++ README.md
@@ -1,24 +1,14 @@
 # pronto-image-classifer
 
-BUILD: docker build -t pronto-image-classifer . --platform linux/amd64
+LaunchPoint for all Drifti R&D projects. 
 
-RUN: docker run --name tf1 --platform linux/amd64 --publish 5000:5000 --rm pronto-image-classifer
+Can either be run with node, or with docker
 
-STOP: docker stop tf1
+node.js:
+- dependencies must be installed, afterwich the application will launch on defaulted port 8080, this can be changed at the top of the index.js file
+- npm install
+- node index.js
 
-TEST: curl -G -d img=https://prod-5929f14.s3.eu-north-1.amazonaws.com/accounts/136/files/ck5du6wwc006c07qx9j596su9 localhost:5000/api/predict OR .../api/predict?img=https%3A%2F%2Fprod-5929f14.s3.eu-north-1.amazonaws.com%2Faccounts%2F136%2Ffiles%2Fck6izzznq01e008oybkak9k8j
-
-TEST_MULTIPLE: curl -i -X POST -H "Content-Type: application/json" -d "{\\\"imgs\\\":[\\\"https://prod-c467854.s3.eu-north-1.amazonaws.com/accounts/128/files/3oXVxlCDCDjxnh9sPYA7g\\\",\\\"https://prod-5929f14.s3.eu-north-1.amazonaws.com/accounts/136/files/ck57u3urw001d07hd6lbe2ciz\\\"]}" localhost:5000/api/predict
-
-multiple sends a post to the same api address, but with an array of img urls in a json object
-
---rm removes the container after it has been stopped
-
-won't run on the plafrom linux/arm64, or the Apple M1 chip
-
-This takes quite a while, a url is uploaded, the file is copied, opened, converted to RGB, then to a tensor. Afterwhich it passes through the model (this part is practially instintaneous)
-
-
-Use the attached create_model.py to retrain the additional model - eventually this may become an endpoint. You can update the model.csv file with the new training data, and then you will be able to run 'python create_model.py' and then rebuild and rerun the docker image.
-In order to run this script you must have python downloaded, and the dependencies installed. You can either see the attached require.txt or run 'python -m pip install --upgrade pip pandas numpy Pillow tensorflow'
-Alternatively you can upload these files to Google Colab which will have all dependencies installed and run the script there
+docker
+- BUILD: docker build -t driftiRD . --platform linux/amd64
+- RUN: docker run --publish 8080:8080 --rm driftiRD
--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const fileUpload = require("express-fileupload");
 const fs = require("fs");
+
+const decode = require("./invoice_files/recognize");
+
+//PORT
+const PORT = 8080
 
 const app = express();
 
@@ -7,15 +13,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", express.static("public"));
-
 app.get('/',(req,res)=>{
     res.send("/public/index.html")
 })
-app.get("/api/csv",(req,res)=>{
-    res.send("files/model.csv")
+
+
+//image
+//
+
+app.use("/image/",express.static("image_public"));
+app.get('/image/',(req,res)=>{
+    res.send("/image_public/index.html")
 })
-app.get("/api/random",(req,res)=>{
-    fs.readFile("files/saveto.csv", "utf-8", function(err, data){
+app.get("/image/api/csv",(req,res)=>{
+    res.send("image_files/model.csv")
+})
+app.get("/image/api/random",(req,res)=>{
+    fs.readFile("image_files/saveto.csv", "utf-8", function(err, data){
         if(err) {
             console.log(err);
         }
@@ -29,8 +43,8 @@ app.get("/api/random",(req,res)=>{
 
 })
 
-app.get("/api/labels",(req,res)=>{
-    fs.readFile("files/model.csv", "utf8",function(err,data){
+app.get("/image/api/labels",(req,res)=>{
+    fs.readFile("image_files/model.csv", "utf8",function(err,data){
         if(err) throw err;
         data = data.split("\r\n")
         newdata = []
@@ -50,12 +64,12 @@ app.get("/api/labels",(req,res)=>{
     
 })
 
-app.get("/api/remove",(req,res)=>{
+app.get("/image/api/remove",(req,res)=>{
     console.log(req.query.label)
-    fs.readFile("files/model.csv", "utf8",function(err,data){
+    fs.readFile("image_files/model.csv", "utf8",function(err,data){
         if(err) throw err;
         data = data.replaceAll(req.query.label," ")
-        fs.writeFile("files/model.csv", data, err => {
+        fs.writeFile("image_files/model.csv", data, err => {
             if (err) {
               console.error(err);
             }
@@ -64,18 +78,92 @@ app.get("/api/remove",(req,res)=>{
     })
 })
 
-app.post("/api/add",(req,res)=>{
+app.post("/image/api/add",(req,res)=>{
     for (let url in req.body){
         line = '\r\n'+url+','+req.body[url].join(' ')
-        fs.writeFile("files/model.csv", line, { flag: 'a' }, err => {
+        fs.writeFile("image_files/model.csv", line, { flag: 'a' }, err => {
             console.log(err)
         });
     }
     res.end()
 })
-app.listen(8080, (error) =>{
+
+
+//quote
+app.use('/quote/',express.static("quote_public"));
+app.get('/quote/',(req,res)=>{
+    res.send('/quote_public/index.html')
+})
+app.put('/quote/api/recommend',(req,res)=>{
+    let rawdata = fs.readFileSync('quote_files/expense_data.txt');
+    let rows = JSON.parse(rawdata);
+    var list = []
+    var inc_list=req.body.items
+    for (let jobID in rows){
+        let items = Object.keys(rows[jobID]["job"])
+        for (var i of inc_list) {
+            if (items.indexOf(i)>=0) {
+                list.push(...items)
+                break;
+            }
+        }
+        
+    }
+    var count={}
+    for (const element of list) {
+        if (count[element]) {
+          count[element] += 1;
+        } else {
+          count[element] = 1;
+        }
+    }
+    for (let el of inc_list){
+        delete count[el]
+    }
+    var keys = Object.keys(count)
+    keys.sort((b,a)=>{return count[a]-count[b]})
+    if (keys.length<5){
+        res.send(keys)
+    }
+    else{
+        res.send(keys.slice(0,5))
+    }
+})
+app.get("/quote/labels",(req,res)=>{
+    let rawdata = fs.readFileSync('quote_files/expense_data.txt');
+    let rows = JSON.parse(rawdata);
+    var list = []
+    for (let jobID in rows){
+        // console.log(jobID)
+        // console.log(rows[jobID]['job'])
+        list.push(...Object.keys(rows[jobID]["job"]))
+    }
+    list = [...new Set(list)]
+    list.sort()
+    list.push("totalItems","sumItems")
+    res.send(list)
+})
+
+//invoice
+app.use(fileUpload());
+app.use('/invoice/',express.static("invoice_public"));
+app.get('/invoice/',(req,res)=>{
+    res.send('/invoice_public/index.html')
+})
+app.post("/invoice/extract-text", (req, res) => {
+    if (!req.files && !req.files.pdfFile) {
+        res.status(400);
+        res.end();
+    }
+    // data = decode(req.files.pdfFile)
+    // res.send(data)
+    decode(req.files.pdfFile).then(data=>res.send(data))
+    
+});
+
+app.listen(PORT, (error) =>{
     if(!error)
-        console.log("Server is Successfully Running, and App is listening on port "+ 8080)
+        console.log("Server is Successfully Running, and App is listening on port "+ PORT)
     else 
         console.log("Error occurred, server can't start", error);
     }
